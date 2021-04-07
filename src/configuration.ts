8d10d61dac9e797ed655be863e1168bd64c81e69
李海涛 <liht@dustess.com>
2021-04-07 09:53:16 +0800

    chore: 初始化代码

---
+++ src/configuration.ts
@@ -0,0 +1,14 @@
+import { App, Configuration } from '@midwayjs/decorator';
+import { ILifeCycle } from '@midwayjs/core';
+import { Application } from 'egg';
+import { join } from 'path';
+
+@Configuration({
+  importConfigs: [join(__dirname, './config')],
+})
+export class ContainerLifeCycle implements ILifeCycle {
+  @App()
+  app: Application;
+
+  async onReady() {}
+}
