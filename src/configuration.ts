a4dabb5c12b198c61fb5bd7b1d7ef41ad6549c30
haitaodesign <lihaitaodesign@gmail.com>
2021-04-22 18:21:12 +0800

    chore: swagger-ui 接入

--- src/configuration.ts
+++ src/configuration.ts
@@ -1,10 +1,14 @@
 import { App, Configuration } from '@midwayjs/decorator';
+import * as swagger from '@midwayjs/swagger';
 import { ILifeCycle } from '@midwayjs/core';
 import { Application } from 'egg';
 import { join } from 'path';
 
 @Configuration({
   importConfigs: [join(__dirname, './config')],
+  imports: [
+    swagger
+  ]
 })
 export class ContainerLifeCycle implements ILifeCycle {
   @App()
